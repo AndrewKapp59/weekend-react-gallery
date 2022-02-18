bf4385535b35179f2b1bc2424063bb19c7f301d5
Andrew Kapp <dkapp59@gmail.com>
2022-02-18 15:44:02 -0600

    Added some images and setup thier paths in the modules.

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,9 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, path: 'images/Anika_in_the_sunflowers.jpg', description: 'Photo of my girlfriend Anika and some sunflowers.', likes: 0 },
+    { id: 3, path: 'images/Drew_and_Wolly_on_dock.jpg', description: 'Photo of my dog wooly and I on a dock', likes: 0 },
+    { id: 4, path: 'images/Ted_fishing.jpg', description: 'Photo of my brother Ted fly fishing.', likes: 0 },
+    { id: 5, path: 'images/Wolly_fall_water.jpg', description: 'One of my favorite photos of my dog Wolly', likes: 0 }
 ];
 
 module.exports = galleryItems;
